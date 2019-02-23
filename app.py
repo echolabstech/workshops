02505f14ee26e7770619c8105d8e9356ebe82520
major <echolabstech@gmail.com>
2019-02-23 11:19:08 +0800

    update stubs with more questions

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ calc = Calculator()
 
 def some_workflow():
 	print('operations are: a(add), s(subtract), m(multiply), d(divide)')
+	## the following line throws an error
+	## TDD test and fix
 	result = calc.run('a', '5', '3')
 	print('the result is:' + str(result))
 	return result
