51e3f201a901dfce8853502ffa0962560e0082fc
major <echolabstech@gmail.com>
2019-02-23 09:52:32 +0800

    stub more of the unanswered code

--- calculator.py
+++ calculator.py
@@ -1,12 +1,15 @@
 from functions import add, subtract, multiply, divide
 
-print('operations are: a(add), s(subtract), m(multiply), d(divide)')
-operator = input()
-if operator == 'a':
-	add()
-elif operator == 's':
-	subtract()
-elif operator == 'm':
-	multiply()
-elif operator == 'd':
-	divide()
+class Calculator():
+	def run(self):
+		print('operations are: a(add), s(subtract), m(multiply), d(divide)')
+		operator = input()
+		if operator == 'a':
+			result = add()
+		elif operator == 's':
+			result = subtract()
+		elif operator == 'm':
+			result = multiply()
+		elif operator == 'd':
+			result = divide()
+		print('the result is:' + str(result))
--- functions.py
+++ functions.py
@@ -8,13 +8,9 @@ def add():
 	print('the result is:' + str(result))
 
 def subtract():
-	print('type a number')
-	left = int(input())
-
-	print('type another number')
-	right = int(input())
-	result = left - right
-	print('the result is:' + str(result))
+	"""
+	implement TDD style
+	"""
 
 def multiply():
 	"""
